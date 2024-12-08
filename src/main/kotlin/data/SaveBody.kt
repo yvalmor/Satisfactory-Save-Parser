4cd89dc7219bd2969377e0b7c5517618c2da9a52
Yvon Morice <yvon.morice5@gmail.com>
2024-12-08 21:12:33 +0100

    finished typing every element

--- src/main/kotlin/data/SaveBody.kt
+++ src/main/kotlin/data/SaveBody.kt
@@ -4,27 +4,19 @@ import data.property.Property
 import data.property.PropertyHeader
 
 typealias PropertyList = List<Pair<PropertyHeader, Property>>
+private typealias Vector3<T> = Triple<T, T, T>
 
 data class SaveFileBody(
     val size: ULong,
     val grids: List<LevelGroupingGrid>,
     val levels: List<Level>,
     val references: List<ObjectReference>,
-) {
-    val subLevelCount: Int
-        get() = levels.size
-
-    val referencesCount: Int
-        get() = references.size
-}
+)
 
 data class LevelGroupingGrid(
     val name: String,
     val levelInfos: List<Pair<String, UInt>>
-) {
-    val levelCount: Int
-        get() = levelInfos.size
-}
+)
 
 data class Level(
     val name: String,
@@ -61,10 +53,6 @@ interface ObjectBody {
     val flag: Boolean
     val size: UInt
     val properties: PropertyList
-    val trailingBytes: List<Byte>
-
-    val propertySize: Int
-        get() = properties.size
 }
 
 data class ActorObject(
@@ -74,16 +62,61 @@ data class ActorObject(
     val parentObjectReference: ObjectReference,
     val components: List<ObjectReference>,
     override val properties: PropertyList,
-    override val trailingBytes: List<Byte>
+    val trailingBytes: List<TrailingBytes>
 ) : ObjectBody {
-    val componentCount: Int
-        get() = components.size
+    interface TrailingBytes
+
+    data class ConveyorTrailingBytesElement(val length: UInt, val name: String, val position: Float)
+    data class CircuitSubsystemTrailingBytesElement(val value: UInt, val reference: ObjectReference)
+    data class VehicleTrailingBytesElement(val value: UInt, val data: List<Byte>)
+    data class LightweightBuildableSubsystemTrailingBytesComponent(
+        val rotation: List<Double>, // 4 Doubles (quaternion)
+        val position: Vector3<Double>,
+        val scale: Vector3<Double>,
+        val swatch: String,
+        val patternDescriptionNumber: String,
+        val primaryColor: List<Float>, // 4 floats (rgba)
+        val secondaryColor: List<Float>, // 4 floats (rgba)
+        val data: List<Byte>,
+        val recipe: String,
+        val blueprintProxy: ObjectReference,
+    )
+
+    data class LightweightBuildableSubsystemTrailingBytesElement(
+        val name: String,
+        val components: List<LightweightBuildableSubsystemTrailingBytesComponent>
+    )
+
+    data class ConveyorChainActorElement(
+        val chainActor: ObjectReference,
+        val belt: ObjectReference,
+        val elements: Vector3<Vector3<ULong>>,
+        val beltIndex: UInt,
+        val items: List<Pair<String, UInt>> // name, count
+    )
+
+    data class ConveyorTrailingBytes(val elements: List<ConveyorTrailingBytesElement>) : TrailingBytes
+    data class GameModeTrailingBytes(val references: List<ObjectReference>) : TrailingBytes
+    data class PlayerStateTrailingBytes(val isEmpty: Boolean, val isSteam: Boolean, val data: List<Byte>) :
+        TrailingBytes
+
+    data class CircuitSubsystemTrailingBytes(val elements: List<CircuitSubsystemTrailingBytesElement>) : TrailingBytes
+    data class PowerLineTrailingBytes(val references: Pair<ObjectReference, ObjectReference>) : TrailingBytes
+    data class VehicleTrailingBytes(val elements: List<VehicleTrailingBytesElement>) : TrailingBytes
+    data class LightweightBuildableSubsystemTrailingBytes(
+        val elements: List<LightweightBuildableSubsystemTrailingBytesElement>
+    ) : TrailingBytes
+    data class ConveyorChainActorTrailingBytes(
+        val startingBelt: ObjectReference,
+        val endingBelt: ObjectReference,
+        val beltSize: UInt,
+        val beltElements: List<ConveyorChainActorElement>,
+    ) : TrailingBytes
 }
 
 data class ComponentObject(
     override val saveVersion: UInt,
     override val flag: Boolean,
     override val size: UInt,
-    override val properties: PropertyList,
-    override val trailingBytes: List<Byte>
-): ObjectBody
+    override val properties: PropertyList
+) : ObjectBody
